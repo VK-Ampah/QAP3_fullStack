df3c8ba01078e9aa977562beb4a804279e17591d
VK-Ampah <valentine.ampah@keyin.com>
2024-03-20 18:49:19 -0230

    setting index page

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+global.DEBUG = true;
+
+global.__basedir = __dirname;
+
+// // middleware
+// app.use((req, res, next) => {
+//     console.log('Time:', Date.now());
+//     next();
+//     });
+
+// configure views and static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    if (DEBUG) console.log('GET /');
+    // res.send('Hello World!');
+    res.render('index');
+    });
+
+app.use((req, res,next) => {
+    res.status(404).send('Sorry cant find that!');
+    });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+);
+
+// Path: public/index.html
