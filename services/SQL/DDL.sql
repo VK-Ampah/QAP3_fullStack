5a2cc5b51dbc4ece900c8bb8707867b38ec553f3
VK-Ampah <valentine.ampah@keyin.com>
2024-03-27 12:50:05 -0230

    add sql queries

--- services/SQL/DDL.sql
+++ services/SQL/DDL.sql
@@ -1,5 +1,7 @@
+-- create database
 CREATE DATABASE soccer;
 
+-- create users table
 CREATE TABLE if not exists users (
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -12,11 +14,23 @@ CREATE TABLE if not exists users (
     CONSTRAINT unique_email UNIQUE (email)  
 );
 
+-- insert user
+INSERT INTO users (first_name, middle_name, last_name, email, username, image_url)
+VALUES ($1, $2, $3, $4, $5, $6)
+RETURNING *;
 
-CREATE TABLE logins (
-    login_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    CONSTRAINT username_fkey FOREIGN KEY (username) REFERENCES users(username) ON UPDATE CASCADE,
-    CONSTRAINT user_unique UNIQUE (username)
-);
+-- get all users
+SELECT * FROM users;
+
+-- get user by id
+SELECT * FROM users WHERE user_id = $1;
+
+-- update user by id    
+UPDATE users 
+SET first_name = $1, middle_name = $2, last_name = $3, email = $4, username = $5, image_url = $6 
+WHERE user_id = $7 
+RETURNING *;
+
+-- delete user by id
+DELETE FROM users WHERE user_id = $1;
+
