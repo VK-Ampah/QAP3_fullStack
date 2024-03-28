14faa122caa5c6aa359642fdb0e0c1f0e6704140
VK-Ampah <valentine.ampah@keyin.com>
2024-03-28 00:17:14 -0230

    minor updates

--- services/dal.auth.js
+++ services/dal.auth.js
@@ -1,11 +1,17 @@
 const Pool = require('pg').Pool
+require('dotenv').config();
+
+
 const pool = new Pool
     ({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'soccer',
-        password: 'Keyin2021',
-        port: 5432,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
     });
 
+
+
 module.exports = pool;
+
